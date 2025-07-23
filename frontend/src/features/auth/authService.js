ab101f9b8945922a1d6f8bf2e239c4c6ab5127dd
ashish25jadhav <92949727+ashish25jadhav@users.noreply.github.com>
2025-07-23 23:05:38 +0530

    Update authService.js

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://todolist-mern-1-backend.onrender.com";
+const API_URL = "https://todolist-mern-1-backend.onrender.com/api/users/";
 
 // Register user
 const register = async (userData) => {
